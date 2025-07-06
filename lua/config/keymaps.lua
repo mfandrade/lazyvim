46265dcd26906419a08ba6224fb2336b49b76996
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-06 14:40:27 -0400

    feat: C-w to save file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- C-w to save file
+-- https://stackoverflow.com/a/77783955/2075507
+vim.keymap.set(
+  { "n" },
+  "<C-w>",
+  ":w<ENTER>:echo 'File saved'<ENTER>",
+  { noremap = true, silent = true, desc = "Save file" }
+)
