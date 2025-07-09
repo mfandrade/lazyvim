278573b77463212cb0a907d9c190f1910a9b3c93
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-09 01:03:00 -0400

    feat: avoid arrow keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Avoid arrow keys
+vim.keymap.set("n", "<Up>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
+
 -- C-w to save file
 -- https://stackoverflow.com/a/77783955/2075507
 vim.keymap.set(
