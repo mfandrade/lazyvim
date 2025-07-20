55c04c05758ce916a48e5e5f05f430e44944b0c5
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-19 23:15:29 -0400

    refactor: arrow keys are an error

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,10 +3,10 @@
 -- Add any additional keymaps here
 
 -- Avoid arrow keys
-vim.keymap.set("n", "<Up>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Down>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Left>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Right>", ":echo 'HJKL!'<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Up>", ":echoerr 'HJKL!'<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", ":echoerr 'HJKL!'<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", ":echoerr 'HJKL!'<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", ":echoerr 'HJKL!'<CR>", { noremap = true, silent = true })
 
 -- C-w to save file -- https://stackoverflow.com/a/77783955/2075507
 vim.keymap.set(
