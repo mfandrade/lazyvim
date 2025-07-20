db8d36abcf53da75d5da26aef76739b96b2a8b49
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-19 23:04:53 -0400

    docs: remove commented keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,6 +19,3 @@ vim.keymap.set(
 -- Copy to clipboard with capital Y -- https://stackoverflow.com/a/67890119/2075507
 vim.keymap.set({ "n" }, "yY", '^"+y$')
 vim.keymap.set({ "n", "v" }, "Y", '"+y')
-
--- Select all text
--- vim.keymap.set({ "n" }, "<C-a>", "ggVG", { noremap = true, silent = true, desc = "Select all text" })
