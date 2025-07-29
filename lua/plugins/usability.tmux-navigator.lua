3d971a2e01214459a8b24d093898e0eabea61145
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-28 22:30:31 -0400

    feat(plugins): vim-tmux-navigator

---
+++ lua/plugins/usability.tmux-navigator.lua
@@ -0,0 +1,22 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<CR>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<CR>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<CR>" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<CR>" },
+    { "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>" },
+  },
+  opts = {
+    tmux_navigator_no_wrap = 1,
+    tmux_navigator_disable_when_zoomed = 1,
+  },
+  config = function() end,
+}
