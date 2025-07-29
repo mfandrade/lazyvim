c4680229d9b5748da744528be7a2efca289bc906
Marcelo F Andrade <mfandrade@gmail.com>
2025-07-28 22:25:46 -0400

    feat(options): good default options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Don't sync clipboard with system
+vim.opt.clipboard = ""
+
+-- Time to differentiate double strokes keymaps
+vim.opt.timeoutlen = 150
+
+-- Easier to read file contents
+vim.opt.scrolloff = 4
+
+-- Enable modelines for flexibility
+vim.opt.modeline = true
+vim.opt.modelines = 1
