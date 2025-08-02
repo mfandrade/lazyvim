273635fdf192f8ec5b0bffa63a4014bc3a8b95b6
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-02 16:48:49 -0400

    fix: keybind for copy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local ops = { noremap = true, silent = true }
+local snr = { silent = true, noremap = true }
 
 local function fish_style_path()
   local fullpath = vim.fn.expand("%:p") -- full path
@@ -24,33 +24,37 @@ local function fish_style_path()
   local abbreviated = table.concat(parts, "/") .. "/" .. filename
   print(abbreviated)
 end
-vim.keymap.set("n", "<C-g>", fish_style_path, ops)
+vim.keymap.set("n", "<C-g>", fish_style_path, snr)
 -- vim.keymap.set("n", "<C-g>", function() print(vim.fn.expand("%:t")) end, ops ) -- ALT
 
 -- Avoid arrow keys
-vim.keymap.set("n", "<Up>", ":echoerr 'HJKL!'<CR>", ops)
-vim.keymap.set("n", "<Down>", ":echoerr 'HJKL!'<CR>", ops)
-vim.keymap.set("n", "<Left>", ":echoerr 'HJKL!'<CR>", ops)
-vim.keymap.set("n", "<Right>", ":echoerr 'HJKL!'<CR>", ops)
+vim.keymap.set("n", "<Up>", ":echoerr 'HJKL!'<CR>", snr)
+vim.keymap.set("n", "<Down>", ":echoerr 'HJKL!'<CR>", snr)
+vim.keymap.set("n", "<Left>", ":echoerr 'HJKL!'<CR>", snr)
+vim.keymap.set("n", "<Right>", ":echoerr 'HJKL!'<CR>", snr)
 
--- Copy to clipboard with capital Y -- https://stackoverflow.com/a/67890119/2075507
-vim.keymap.set({ "n" }, "yY", '^"+y$', ops)
-vim.keymap.set({ "n", "v" }, "Y", '"+y', ops)
+-- Copy to clipboard with capital Y
+vim.keymap.set("v", "Y", '"+y', snr)
+vim.keymap.set("n", "YY", function()
+  local curpos = vim.api.nvim_win_get_cursor(0)
+  vim.cmd('normal! ^"+yg_')
+  vim.api.nvim_win_set_cursor(0, curpos)
+end, snr)
 
 -- Less hand movement -- https://vi.stackexchange.com/a/16969
-vim.keymap.set("i", "hj", "<ESC>", ops)
-vim.keymap.set("n", "jj", ":w<CR>:echo 'File saved'<CR>", ops)
-vim.keymap.set("n", "aa", "ggVG", ops)
-vim.keymap.set("n", "AA", 'ggVG"+y', ops)
-vim.keymap.set("n", "QQ", ":qall<CR>", ops)
-vim.keymap.set("n", "0", "^", ops)
-vim.keymap.set("n", "00", "0", ops)
+vim.keymap.set("i", "hj", "<ESC>", snr)
+vim.keymap.set("n", "jj", ":w<CR>:echo 'File saved'<CR>", snr)
+vim.keymap.set("n", "aa", "ggVG", snr)
+vim.keymap.set("n", "AA", 'ggVG"+y', snr)
+vim.keymap.set("n", "QQ", ":qall<CR>", snr)
+vim.keymap.set("n", "0", "^", snr)
+vim.keymap.set("n", "00", "0", snr)
 
 -- F-keys
 -- F1 = doc underlying symbol
 -- F2 = rename
-vim.keymap.set("n", "<F3>", "<C-e>", ops)
-vim.keymap.set("n", "<F4>", "<C-y>", ops)
+vim.keymap.set("n", "<F3>", "<C-e>", snr)
+vim.keymap.set("n", "<F4>", "<C-y>", snr)
 -- F5 = dap continue
 -- F6 = dap step over
 -- F7 = dap step into
