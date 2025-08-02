5b56b36f9ac55e6bc5d0f23dc7574a74d18ebd1e
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-01 21:00:52 -0400

    chore: reorganize plugins

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    -- seemlessly navigation through vim and tmux panes
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<CR>" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<CR>" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<CR>" },
+      { "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>" },
+    },
+    opts = {
+      tmux_navigator_no_wrap = 1,
+      tmux_navigator_disable_when_zoomed = 1,
+      tmux_navigator_save_on_switch = 2,
+    },
+    config = function() end,
+  },
+  -- allows session restore with tmux-resurrect
+  { "tpope/vim-obsession" },
+}
