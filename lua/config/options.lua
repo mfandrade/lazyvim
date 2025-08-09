bd0e9c05115472fdad9172f5da938e537520ecce
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-09 12:04:01 -0400

    fix:🩹 two recommendations found in LazyHealth

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 -- Don't sync clipboard with system
 vim.opt.clipboard = ""
 
