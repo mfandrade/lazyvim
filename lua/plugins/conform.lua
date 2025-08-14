ed634a07f818327f568c25faf6e4984b083fbbc4
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-14 18:10:44 -0400

    feat: set prettier as default formatter for js, ts and others

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,7 +4,13 @@ return {
   opts = {
     formatters_by_ft = {
       sh = { "shfmt" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      json = { "prettier" },
+      markdown = { "prettier" },
+      yaml = { "prettier" },
     },
+    format_on_save = false,
     formatters = {
       shfmt = function(bufnr)
         return {
@@ -12,6 +18,15 @@ return {
           args = { "-i", tostring(vim.bo[bufnr].shiftwidth), "-ci" },
         }
       end,
+      prettier = {
+        command = "prettier",
+        args = {
+          "--stdin-filepath",
+          "$FILENAME",
+        },
+        stdin = true,
+        cwd = require("conform.util").root_file({ ".prettierrc", "package.json", ".git" }),
+      },
     },
   },
 }
