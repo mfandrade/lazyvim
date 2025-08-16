72602f47f6c2f1984668e2d462b0a29b693571bf
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-16 00:29:59 -0400

    refactor: simplify clock removal in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,20 +2,9 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = function(_, opts)
-    local catppuccin = require("catppuccin.palettes").get_palette()
     opts.sections = opts.sections or {}
-    opts.sections.lualine_z = {}
-    opts.options = opts.options or {}
+    opts.sections.lualine_z = {} -- removes clock
 
-    if type(opts.options.theme) == "string" then
-      local theme_name = opts.options.theme
-      opts.options.theme = require("lualine.themes." .. theme_name) or {}
-    end
-
-    opts.options.theme = opts.options.theme or {}
-    opts.options.theme.insert = {
-      a = { fg = catppuccin.red, bg = catppuccin.base },
-    }
     return opts
   end,
 }
