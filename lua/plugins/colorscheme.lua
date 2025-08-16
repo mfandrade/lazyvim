00b763caafe9f8aef9cd5ed4c41f5db975063662
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-16 00:29:26 -0400

    feat: toggle transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,14 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    opts = {
-      transparent_background = true,
-    },
+    lazy = false,
+    priority = 1000,
   },
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = true,
       styles = {
         sidebars = "dark",
         floats = "dark",
@@ -32,14 +30,41 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+  },
+  {
+    "xiyaowong/transparent.nvim",
     opts = {
-      transparent_background = true,
+      groups = {
+        "Normal",
+        "NormalNC",
+        "NvimTreeNormal",
+      },
+      extra_groups = {},
+      exclude_groups = {
+        "NormalFloat",
+        "TelescopeNormal",
+        "TelescopePromptNormal",
+        "MasonNormal",
+        "LspInfoBorder",
+      },
     },
+    config = function(_, opts)
+      require("transparent").setup(opts)
+
+      -- fix which-key border with gruvbox
+      vim.cmd([[
+      augroup GruvboxTransparentFix
+        autocmd!
+        autocmd ColorScheme gruvbox highlight FloatBorder guibg=NONE guifg=#928374
+        autocmd ColorScheme gruvbox highlight WhichKeyBorder guibg=NONE guifg=#928374
+      augroup END
+    ]])
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "tokyonight", --TODO: set the colorscheme
     },
   },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,6 +8,19 @@ return {
       callback = function()
         -- Create some toggle mappings
         require("snacks").toggle.option("list", { name = "Invisible Chars" }):map("<leader>uv")
+        require("snacks").toggle
+          .new({
+            id = "transparent_toggle",
+            name = "Transparent Background",
+            get = function()
+              local bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+              return bg == nil
+            end,
+            set = function(state)
+              vim.cmd("TransparentToggle")
+            end,
+          })
+          :map("<leader>uB")
       end,
     })
   end,
