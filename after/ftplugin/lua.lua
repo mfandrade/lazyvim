3ca014f44dff3a7e2cc39106e6f1a297f47541e9
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-19 01:47:43 -0400

    feat: readd ftplugins

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", ",,", "A,", { noremap = true, silent = true, desc = "Append a , at the end of the line" })
---
+++ after/ftplugin/typescript.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", ";;", "A;<ESC><CR>", { noremap = true, silent = true, desc = "Append a ; at the end of the line" })
