c5f33034cb54cc25c08e7c88259eb30778664af6
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-19 01:49:33 -0400

    refactor: remove bool in favor of dial

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,29 +1,5 @@
 return {
-  { "HiPhish/rainbow-delimiters.nvim" },
-  {
-    "nil70n/floating-help",
-    opts = {
-      border = "rounded", -- solid, none, single, double, rounded, shadow
-      ratio = 0.7,
-    },
-  },
-  {
-    "nat-418/boole.nvim",
-    opts = {
-      mappings = {
-        increment = "<C-a>",
-        decrement = "<C-x>",
-      },
-      additions = {
-        { "sun", "mon", "tue", "wed", "thu", "fri", "sat" },
-        { "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec" },
-      },
-      allow_caps_additions = {
-        { "true", "false" },
-        { "sun", "mon", "tue", "wed", "thu", "fri", "sat" },
-        { "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec" },
-      },
-    },
-  },
   { "xiyaowong/virtcolumn.nvim" },
+  { "HiPhish/rainbow-delimiters.nvim" },
+  { "nil70n/floating-help", opts = { border = "rounded" } },
 }
