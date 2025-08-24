53e434a001d85cf59dc372a45c0760be280cd31d
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-24 00:52:47 -0400

    feat: include lua and fish to format on save

--- lua/plugins/editor.conform.lua
+++ lua/plugins/editor.conform.lua
@@ -4,18 +4,19 @@ return {
   opts = {
     formatters_by_ft = {
       sh = { "shfmt" },
+      lua = { "stylua" },
+      fish = { "fish_indent" },
       javascript = { "prettier" },
       typescript = { "prettier" },
       json = { "prettier" },
-      markdown = { "prettier" },
-      yaml = { "prettier" },
     },
     format_on_save = false,
     formatters = {
+      injected = { options = { ignore_errors = true } },
       shfmt = function(bufnr)
         return {
           command = "shfmt",
-          args = { "-i", tostring(vim.bo[bufnr].shiftwidth), "-ci" },
+          args = { "-i", tostring(vim.bo[bufnr].shiftwidth or 2), "-ci" },
         }
       end,
       prettier = {
