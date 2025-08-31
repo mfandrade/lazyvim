9d24612aebf935b78d5272b2034345d4b928a78d
Marcelo F Andrade <mfandrade@gmail.com>
2025-08-31 11:02:21 -0400

    style: more bright insert mode in vim

--- lua/plugins/ui.lualine.lua
+++ lua/plugins/ui.lualine.lua
@@ -1,22 +1,43 @@
+local theme_name = vim.g.colors_name or "auto"
+
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  opts = {
-    sections = {
-      lualine_c = {
-        { "fileformat" },
-        -- { "filetype" },
-        {
-          "buffers",
-          hide_filename_extension = false,
-          mode = 4,
-          symbols = {
-            modified = " ",
-            alternate_file = "",
+
+  opts = function()
+    local ok, theme = pcall(require, "lualine.themes." .. theme_name)
+    if ok then
+      theme.insert.a.bg = "#990000"
+      theme.insert.a.fg = "#CCCCCC"
+    end
+    return {
+      options = {
+        theme = ok and theme or "auto",
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            fmt = function(str)
+              return string.sub(str, 1, 3):upper()
+            end,
           },
         },
+        lualine_c = {
+          { "fileformat" },
+          -- { "filetype" },
+          {
+            "buffers",
+            hide_filename_extension = false,
+            mode = 4,
+            symbols = {
+              modified = " ",
+              alternate_file = "",
+            },
+          },
+        },
+        lualine_z = {},
       },
-      lualine_z = {},
-    },
-  },
+    }
+  end,
 }
