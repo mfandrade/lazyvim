0045e71aa6866497682fc38f31ea260c88834edd
Marcelo F Andrade <mfandrade@gmail.com>
2026-01-24 20:59:41 -0300

    feat(options): listchars

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,18 @@
 ---@diagnostic disable: undefined-global
 
 vim.opt.textwidth = 120
+
+-- stylua: ignore start
+vim.opt.list = true
+vim.opt.listchars = {
+  tab      = "» ",
+  space    = "·",
+  trail    = "⨉",
+  nbsp     = "␣",
+  extends  = "…",
+  precedes = "…",
+  eol      = "⏎",
+}
+vim.opt.showbreak = "↪ "
+vim.opt.breakindent = true
+-- stylua: ignore end
