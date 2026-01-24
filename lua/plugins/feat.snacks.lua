0da493ce3ea351804262efb83c3d82300cf884a1
Marcelo F Andrade <mfandrade@gmail.com>
2026-01-24 18:04:30 -0300

    style(plugins): snacks pick explorer to the right

--- lua/plugins/feat.snacks.lua
+++ lua/plugins/feat.snacks.lua
@@ -2,5 +2,16 @@ return {
   "folke/snacks.nvim",
   opts = {
     animate = { enabled = false },
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
   },
 }
