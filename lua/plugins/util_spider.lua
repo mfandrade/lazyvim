4f5f08a38ad05457f0a7732501d117b95aee8e01
Marcelo F Andrade <mfandrade@gmail.com>
2026-01-25 19:08:29 -0300

    feat(plugins): reenable a fixed version of keymap for precognition

--- lua/plugins/util_spider.lua
+++ lua/plugins/util_spider.lua
@@ -9,4 +9,31 @@ return {
     },
     opts = {},
   },
+  {
+    "tris203/precognition.nvim",
+    event = "VeryLazy",
+    opts = { startVisible = false },
+    config = function(_, opts)
+      local pre = require("precognition")
+      pre.setup(opts)
+
+      -- Rastreia o estado internamente para o toggle não se perder
+      local is_visible = opts.startVisible
+
+      Snacks.toggle({
+        name = "Precognition",
+        get = function()
+          return is_visible
+        end,
+        set = function(state)
+          is_visible = state
+          if state then
+            pre.show()
+          else
+            pre.hide()
+          end
+        end,
+      }):map("<leader>up")
+    end,
+  },
 }
