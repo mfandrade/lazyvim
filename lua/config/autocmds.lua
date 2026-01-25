aadb23815e09f82f205f8fd9b7a79055737116f9
Marcelo F Andrade <mfandrade@gmail.com>
2026-01-24 21:57:30 -0300

    feat(autocmds): remove two default lazyvim autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,20 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- * lazyvim_checktime
+-- * lazyvim_highlight_yank
+-- * lazyvim_resize_splits
+-- * lazyvim_last_loc
+-- * lazyvim_close_with_q
+-- * lazyvim_man_unlisted
+-- * lazyvim_wrap_spell
+-- * lazyvim_json_conceal
+-- * lazyvim_auto_create_dir
 --
 -- Remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+
+vim.api.nvim_del_augroup_by_name("lazyvim_close_with_q")
+vim.api.nvim_del_augroup_by_name("lazyvim_json_conceal")
+
 -- Or add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
 --
