9556d42fc7566aff23413e1e944e51bae4345eaa
Marcelo de Freitas Andrade <mfandrad@motorola.com>
2026-01-26 16:36:12 -0400

    fix(plugins): avoid lowercase roman increments in speeddating

--- lua/plugins/util_speeddating.lua
+++ lua/plugins/util_speeddating.lua
@@ -2,6 +2,17 @@
 return {
   "tpope/vim-speeddating",
   config = function()
-    vim.g.speeddating_no_roman = 1
+    vim.g.speeddating_no_roman = 0 -- 1 if you want to disable roman support
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      group = vim.api.nvim_create_augroup("ignore_speeddating_romans", { clear = true }),
+      pattern = "*",
+      callback = function()
+        if vim.fn.exists(":SpeedDatingFormat") > 0 then
+          vim.cmd("SpeedDatingFormat! %v")
+          vim.cmd([[SpeedDatingFormat \b[IVXLCDM]\+\b]])
+        end
+      end,
+    })
   end,
 }
