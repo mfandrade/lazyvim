bdeb0269c464155396726c77fa63feb32c4264d9
Marcelo F Andrade <mfandrade@gmail.com>
2026-01-31 17:16:19 -0300

    feat: keymaps and options

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,1 +1,195 @@
-vim.keymap.set("i", "kj", "<esc>", { desc = "Normal mode", silent = true, noremap = true })
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+---@diagnostic disable: undefined-global
+
+local function map(keycomb, target, desc, mode, opts)
+  local options = opts or { noremap = true, silent = true }
+  options.desc = desc
+  vim.keymap.set(mode or "n", keycomb, target, options)
+end
+
+-- help
+-- map("<f1>", function()
+--   Snacks.picker.help()
+-- end, "Help")
+map("<f1><f1>", function()
+  local cw = vim.fn.expand("<cword>")
+  if cw ~= "" and vim.fn.hlexists(cw) == 0 then
+    local ok = cw ~= "" and pcall(function()
+      vim.cmd("help " .. cw)
+    end)
+    if not ok then
+      vim.cmd("help")
+    end
+  end
+end, "Help!")
+
+-- normal mode
+map("kj", "<esc>", "Escape", "i")
+map("0", "^", "Begining of the line (non-blank chars)")
+map("00", "0", "Begining of the line")
+
+-- saves and quits
+map("<leader>w", "<cmd>write<cr>", "Save")
+map("<leader>W", "<cmd>noautocmd write<cr>", "Save (no autocmds)")
+map("<leader>q", "<cmd>quit<cr>", "Quit")
+map("<leader>Q", "<cmd>quitall!<cr>", "Quit without saving")
+map("<leader>X", "<cmd>xit!<cr>", "Save and quit")
+
+-- appends
+map(",,", "A,<esc>", "Append ,")
+map(";;", "A;<esc>", "Append ;")
+map("\\\\", "A \\<esc>", "Append \\")
+
+-- no save to clipboard
+map("x", '"_dl', "Delete right char")
+map("X", '"_dh', "Delete left char")
+map("_", '"_d', "Delete motion")
+map("_", '"_d', "Delete selection", "v")
+map("__", '"_dd', "Delete line")
+map("c", '"_c', "Change motion")
+map("c", '"_c', "Change selection", "x")
+map("C", '"_c', "Change selection", "x")
+map("cc", '"_cc', "Change current line")
+map("C", '"_c$', "Change the rest of the line")
+map("s", '"_cl', "Substitute char")
+map("s", '"_cc', "Substitute selection", "x")
+map("S", '"_S', "Substitute line")
+
+-- system clipboard in visual mode
+map("D", "d$", "Delete the rest of the line")
+map("D", '"+d', "Delete to clipboard", "v")
+map("Y", "y$", "Yank the rest of the line")
+map("Y", '"+y', "Yank to clipboard", "v")
+map("<leader>Y", "<cmd>%yank +<cr>", "Yank all text to clipboard") -- no need to select all text
+
+-- maintain selection
+map("<", "<gv", "Indent left", "v")
+map(">", ">gv", "Indent right", "v")
+map("<", "<<", "Indent left")
+map(">", ">>", "Indent right")
+
+-- centralize
+map("n", "nzzzv", "Next result")
+map("N", "Nzzzv", "Prev result")
+map("<c-d>", "<c-d>zz", "Scroll down")
+map("<c-u>", "<c-u>zz", "Scroll up")
+
+-- move lines
+map("<a-j>", "<cmd>m .+1<cr>==", "Move down")
+map("<a-k>", "<cmd>m .-2<cr>==", "Move up")
+map("<a-j>", ":m '>+1<cr>gv=gv", "Move lines down", "v")
+map("<a-k>", ":m '<-2<cr>gv=gv", "Move lines up", "v")
+map("<a-j>", "<esc><cmd>m .+1<cr>==gi", "Move down", "i")
+map("<a-k>", "<esc><cmd>m .-2<cr>==gi", "Move up", "i")
+
+-- folding
+map("+", "zo", "Open fold")
+map("-", "zc", "Close fold")
+map("z+", "zR", "Open all folds in file")
+map("z-", "zM", "Close all folds in file")
+
+-- splits / vim-tmux-navigator
+local function tmux_navigate(direction)
+  local win = vim.api.nvim_get_current_win()
+  vim.cmd("wincmd " .. direction)
+  if win == vim.api.nvim_get_current_win() then
+    local tmux_dir = { h = "L", j = "D", k = "U", l = "R" }
+    os.execute("tmux select-pane -" .. tmux_dir[direction])
+  end
+end
+-- stylua: ignore start
+map("<c-h>", function() tmux_navigate("h") end, "Navigate left")
+map("<c-j>", function() tmux_navigate("j") end, "Navigate down")
+map("<c-k>", function() tmux_navigate("k") end, "Navigate up")
+map("<c-l>", function() tmux_navigate("l") end, "Navigate right")
+-- stylua: ignore end
+
+-- arrows
+local keys = { "left", "right", "up", "down", "pageup", "pagedown", "home", "end" }
+local modes = { "n", "v", "o", "x", "i", "s" }
+for _, mode in ipairs(modes) do
+  for _, key in ipairs(keys) do
+    map("<" .. key .. ">", "<nop>", "Hard mode", mode)
+    map("<c-" .. key .. ">", "<nop>", "Hard mode Ctrl", mode)
+  end
+end
+
+local arrows = { h = "left", j = "down", k = "up", l = "right" }
+local nav_modes = { "v", "o", "x", "i", "s" }
+for _, mode in ipairs(nav_modes) do
+  for key, dir in pairs(arrows) do
+    map("<c-" .. key .. ">", "<" .. dir .. ">", "Cursor " .. dir, mode)
+  end
+end
+
+-- stylua: ignore start
+local function move_no_scrolloff(key)
+  local value = vim.o.scrolloff
+  vim.opt.scrolloff = 0
+  vim.cmd("normal! " .. key)
+  vim.opt.scrolloff = value
+end
+map("H", function() move_no_scrolloff("H") end, "Home line") -- vim.keymap.del("n", "H")
+map("L", function() move_no_scrolloff("L") end, "Last line") -- vim.keymap.del("n", "L")
+map("'", "<nop>", nil)
+map("M", "'", "Go to mark")
+-- map("M", "M", "Mid line") -- do it as Hzz or Lzz
+-- stylua: ignore end
+
+-- buffer navigation
+-- map("<tab>", vim.cmd.bnext, "Next buffer")
+-- map("<s-tab>", vim.cmd.bprev, "Prev buffer")
+-- for i = 1, 5 do
+--   map("<tab>" .. i, function()
+--     local bufs = vim.tbl_filter(function(buf)
+--       return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.bo[buf].buftype == ""
+--     end, vim.api.nvim_list_bufs())
+--     if bufs[i] then
+--       vim.api.nvim_set_current_buf(bufs[i])
+--     end
+--   end, "Buffer #" .. i)
+-- end
+-- map("<tab><tab>", function()
+--   Snacks.picker.buffers()
+-- end, "Open buffers")
+--
+-- stylua: ignore start
+-- Snacks.toggle({ name = "Invisible Chars",
+--   get = function() return vim.wo.list end,
+--   set = function(state) vim.wo.list = state end,
+-- }):map("<leader>uv")
+
+-- Snacks.toggle({ name = "Highlight Cursorline",
+--   get = function() return vim.wo.cursorline end,
+--   set = function(state) vim.wo.cursorline = state end,
+-- }):map("<leader>uH")
+
+-- Snacks.toggle({ name = "Auto-wrap",
+--   get = function() return vim.wo.colorcolumn ~= "" end,
+--   set = function(state)
+--     local tw = vim.bo.textwidth
+--     if state then
+--       if tw > 0 then
+--         vim.opt_local.formatoptions:append({ "t", "c" })
+--         vim.wo.colorcolumn = tostring(tw) or "120"
+--       end
+--     else
+--       vim.opt_local.formatoptions:remove({ "t", "c" })
+--       vim.wo.colorcolumn = ""
+--     end
+--   end,
+-- }):map("<leader>ua")
+
+-- Snacks.toggle({ name = "Transparency",
+--     get = function() return vim.g.transparent_enabled end,
+--     set = function(state)
+--       if state then vim.cmd("TransparentEnable")
+--       else vim.cmd("TransparentDisable") end
+--     end,
+-- }):map("<leader>ut")
+
+-- Snacks.toggle({ name = "Mini Pairs",
+--   get = function() return not vim.g.minipairs_disable end,
+--   set = function(state) vim.g.minipairs_disable = not state end,
+-- }):map("<leader>uP")
+-- -- stylua: ignore end
--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,83 @@
-vim.opt.number = true
-vim.opt.modelines = 2
-vim.opt.modeline = true
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+---@diagnostic disable: undefined-global
+-- stylua: ignore start
+
+vim.g.have_nerd_font = true
+
+local conf = vim.opt
+conf.clipboard = "unnamed"			 -- Don't sync with system clipboard
+conf.mouse = "a"					 -- Enable mouse support for all modes
+conf.tildeop = true					 -- Make tilde (~) behave like an operator
+conf.updatetime = 250				 -- Faster completion and trigger for CursorHold
+conf.timeoutlen = 500				 -- Time to wait for a mapped sequence to complete
+conf.scrolloff = 5					 -- Minimum number of lines to keep above/below the cursor
+conf.modeline = true				 -- Modelines are useful for per-file settings
+conf.modelines = 2					 -- How many lines at bottom/top to check
+
+local line = vim.opt
+line.number = true					 -- Show absolute line number
+line.relativenumber = true			 -- Show relative line number
+
+local editor = vim.opt
+editor.cmdheight = 0				 -- Use 0 lines (moke more room for code)
+editor.laststatus = 3				 -- Global status bar (just one in background, regardless splits)
+editor.signcolumn = "yes"			 -- Always show the sign column to prevent text shifting
+editor.showtabline = 0				 -- Never show the tab line
+editor.showmode = false				 -- Hide mode (e.g. -- INSERT --) as it's often in statusline
+editor.cursorline = true			 -- Highlight the current line
+editor.colorcolumn = "+1"			 -- Draw cursorcolumn 1 char after textwidth
+
+local panes = vim.opt
+panes.splitright = true				 -- Vertical splits will automatically be on the right
+panes.splitbelow = true				 -- Horizontal splits will automatically be below
+
+local indent = vim.opt
+indent.expandtab = true				 -- Convert tabs to spaces
+indent.smarttab = true				 -- Make tab inserting smarter
+indent.autoindent = true			 -- Copy indent from current line when starting a new one
+indent.smartindent = true			 -- Insert indents automatically in specific contexts
+indent.list = false					 -- Default to not show invisible chars
+indent.listchars = {				 -- How to show invisible chars
+  tab      = "» ",
+  space    = "·",
+  trail    = "⨉",
+  nbsp     = "␣",
+  extends  = "…",
+  precedes = "…",
+  eol      = "⏎",
+}
+
+local wrapping = vim.opt
+wrapping.textwidth = 120			 -- The length of the text that fits in a single line with no wrap (when enabled)
+wrapping.wrap = false				 -- No wrap long lines by default
+wrapping.breakindent = true			 -- Wrapped lines will retain the same indent
+wrapping.showbreak = "↪ "			 -- String to put at the start of lines that have been wrapped
+
+local search = vim.opt
+search.ignorecase = true			 -- Ignore case in search patterns
+search.smartcase = true				 -- Override 'ignorecase' if search pattern contains upper case
+search.inccommand = "nosplit"		 -- Show live preview of substitution in a split window
+
+local folding = vim.opt
+folding.fillchars:append({ foldopen = "", foldclose = "", foldsep = "|", fold = " " })
+folding.foldtext = ""							 -- Character to connect foldable lines
+folding.foldlevel = 99							 -- Open all folds by default
+folding.foldcolumn = "0"						 -- Fold will be handled by Snacks
+folding.foldmethod = "expr"						 -- Use expression for folding
+folding.foldexpr = "v:lua.vim.lsp.foldexpr()"	 -- Use LSP for folding
+folding.viewoptions = "folds,cursor"			 -- When using view, folds are persisted
+folding.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "folds" }
+
+local undo = vim.opt
+undo.undofile = true				 -- Save undo history to a file so it persists after closing
+undo.undolevels = 10000				 -- Maximum number of changes that can be undone
+
+-- Backups and swap
+local backup = vim.opt
+backup.autowriteall = true			 --	To avoid annoying confirmation dialogs
+backup.backup = false				 -- Don't create a backup file before overwriting a file
+backup.writebackup = false			 -- Don't make a backup before overwriting a file
+backup.swapfile = false				 -- Don't use swapfiles (can be risky, but cleaner)
+
+-- stylua: ignore end
+-- vim:noet:ts=4:sw=4:sts=4
