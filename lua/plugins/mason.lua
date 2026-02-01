4735a485d98611995bdee69a6d83b1e1d913b302
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-01 00:36:04 -0300

    feat(plugins): add workspace-diagnostics

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,11 +2,17 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "artemave/workspace-diagnostics.nvim",
   },
   opts = {
     servers = { -- NOTE: you must set your Mason language servers here!
       lua_ls = { settings = { Lua = { diagnostics = { globals = { "vim" } } } } },
       stylua = {},
+      ts_ls = {
+        on_attach = function(client, _bufnr)
+          require("workspace-diagnostics").populate_workspace_diagnostics(client, 0)
+        end,
+      },
     },
   },
   config = function(_, opts)
