576a39c64d31352f8b6da82819d019210d3a99ab
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-01 00:06:29 -0300

    style(plugins): rounded border in autocompletion

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,20 +1,27 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-
-  version = '1.*',
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
+  "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  version = "1.*",
   opts = {
-    keymap = { preset = 'enter' },
-    appearance = {
-      nerd_font_variant = 'mono'
+    keymap = { preset = "enter" },
+    appearance = { nerd_font_variant = "mono" },
+    completion = {
+      menu = {
+        border = "rounded",
+        draw = { gap = 0 },
+        winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+        window = {
+          border = "rounded",
+          winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+        },
+      },
     },
-    completion = { documentation = { auto_show = false } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    sources = { default = { "lsp", "path", "snippets", "buffer" } },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
   },
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
