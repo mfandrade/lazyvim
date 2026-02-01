d92dfd04b3dec65345195c5728906db3e5bb2564
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-01 00:09:14 -0300

    feat(plugins): add treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,66 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = { "mks-h/treesitter-autoinstall.nvim" },
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require("treesitter-autoinstall").setup()
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "mks-h/treesitter-autoinstall.nvim" },
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("treesitter-autoinstall").setup()
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    opts = {
+      move = {
+        enable = true,
+        set_jumps = true,
+        keys = {
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        },
+      },
+    },
+    config = function(_, opts)
+      local TS = require("nvim-treesitter-textobjects")
+      TS.setup(opts)
+
+      local function attach(buf)
+        local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
+        if not lang then
+          return
+        end
+
+        local moves = vim.tbl_get(opts, "move", "keys") or {}
+
+        for method, keymaps in pairs(moves) do
+          for key, query in pairs(keymaps) do
+            local part = query:gsub("@", ""):gsub("%..*", "")
+            part = part:sub(1, 1):upper() .. part:sub(2)
+            local desc = (key:sub(1, 1) == "[" and "Prev " or "Next ") .. part
+            desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and " End" or " Start")
+
+            if not (vim.wo.diff and key:find("[cC]")) then
+              vim.keymap.set({ "n", "x", "o" }, key, function()
+                require("nvim-treesitter-textobjects.move")[method](query, "textobjects")
+              end, { buffer = buf, desc = desc, silent = true })
+            end
+          end
+        end
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("user_treesitter_textobjects", { clear = true }),
+        callback = function(ev)
+          attach(ev.buf)
+        end,
+      })
+
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        attach(buf)
+      end
+    end,
+  },
 }
