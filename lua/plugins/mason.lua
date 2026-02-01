f21ff1b151d3383d5178e81f550249b20e733606
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-01 00:06:55 -0300

    feat(plugins): drop lspconfig as it is builtin in nvim 0.12-dev

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,28 +1,20 @@
-local lua = { "lua_ls", "stylua" }
-local js = { "vtsls", "prettier" }
-local py = {}
-
-local servers = vim.iter({ lua, js, py }):flatten():totable()
 return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
   },
   opts = {
-    servers = {
-      lua_ls = {
-        settings = { Lua = { diagnostics = { globals = { "vim" } } } }
-      },
-      stylua = {}
+    servers = { -- NOTE: you must set your Mason language servers here!
+      lua_ls = { settings = { Lua = { diagnostics = { globals = { "vim" } } } } },
+      stylua = {},
     },
   },
   config = function(_, opts)
     require("mason").setup()
-    require("mason-lspconfig").setup({ ensure_installed = servers })
+    require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(opts.servers) })
     for server, config in pairs(opts.servers) do
       vim.lsp.config(server, config)
       vim.lsp.enable(server)
     end
-  end
+  end,
 }
