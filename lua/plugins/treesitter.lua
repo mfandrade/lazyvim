eda7b727422a67744c9ab76c36c57441ece2d75c
Marcelo F Andrade <mfandrade@gmail.com>
2026-01-31 21:38:12 -0300

    feat(plugins): proper treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,14 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
+  "nvim-treesitter/nvim-treesitter",
   lazy = false,
-  build = ':TSUpdate'
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.config").setup({
+      auto_install = true,
+      sync_install = false,
+      indent = { enable = true },
+      highlight = { enable = true },
+      ensure_installed = "maintained",
+    })
+  end,
 }
