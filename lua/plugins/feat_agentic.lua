baa8306c717916e4d182a2c26fe45ffa11129bfb
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-02 21:25:49 -0300

    feat(plugins): agentic ai

---
+++ lua/plugins/feat_agentic.lua
@@ -0,0 +1,15 @@
+return {
+  "carlos-algms/agentic.nvim",
+
+  opts = {
+    -- Available by default: "claude-acp" | "gemini-acp" | "codex-acp" | "opencode-acp" | "cursor-acp" | "auggie-acp"
+    provider = "gemini-acp", -- setting the name here is all you need to get started
+  },
+
+  -- stylua: ignore
+  keys = {
+    { "<C-\\>", function() require("agentic").toggle() end, mode = { "n", "v", "i" }, desc = "Toggle Agentic Chat", },
+    { "<C-'>", function() require("agentic").add_selection_or_file_to_context() end, mode = { "n", "v" }, desc = "Add file or selection to Agentic to Context", },
+    { "<C-,>", function() require("agentic").new_session() end, mode = { "n", "v", "i" }, desc = "New Agentic Session", },
+  },
+}
