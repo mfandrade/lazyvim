5796158e0818358f44cb3cc0ded8b055bea86455
Marcelo de Freitas Andrade <mfandrad@motorola.com>
2026-02-04 15:37:32 -0400

    style(ftplugins): increase default tabstop size for makefile

--- lua/after/ftplugin/make.lua
+++ lua/after/ftplugin/make.lua
@@ -1,6 +1,6 @@
 vim.opt_local.expandtab = false -- IMPORTANT!
 vim.opt_local.list = true
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 8
+vim.opt_local.softtabstop = 8
+vim.opt_local.tabstop = 8
 vim.opt_local.conceallevel = 0
