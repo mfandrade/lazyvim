f69e4cd775b0f61911d4dcda40100666152acf78
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-05 21:38:36 -0300

    feat(plugins): improve codecompanion

--- lua/plugins/feat_codecompanion.lua
+++ lua/plugins/feat_codecompanion.lua
@@ -1,72 +1,48 @@
 return {
-  {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "j-hui/fidget.nvim",
-    },
-    opts = {
-      strategies = {
-        chat = {
-          adapter = "gemini",
-        },
-        inline = {
-          adapter = "gemini",
-        },
-      },
-      adapters = {},
-    },
-    config = function(_, opts)
-      local api_key = ""
-      local handle = io.popen("pass gemini/api_key")
-      if handle then
-        api_key = handle:read("*a"):gsub("%s+$", "")
-        handle:close()
-      end
-
-      opts.adapters.gemini = function()
-        return require("codecompanion.adapters").extend("gemini", {
-          env = {
-            api_key = api_key,
-          },
-        })
-      end
-
-      require("codecompanion").setup(opts)
-
-      local progress = require("fidget.progress")
-      local handles = {}
-      local group = vim.api.nvim_create_augroup("CodeCompanionFidget", {})
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "CodeCompanionRequestStarted",
-        group = group,
-        callback = function(e)
-          handles[e.data.id] = progress.handle.create({
-            title = "CodeCompanion",
-            message = "Thinking...",
-            lsp_client = { name = e.data.adapter.formatted_name },
-          })
-        end,
-      })
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "CodeCompanionRequestFinished",
-        group = group,
-        callback = function(e)
-          local h = handles[e.data.id]
-          if h then
-            h.message = e.data.status == "success" and "Done" or "Failed"
-            h:finish()
-            handles[e.data.id] = nil
-          end
-        end,
-      })
-    end,
-  },
-  {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "j-hui/fidget.nvim",
     "MeanderingProgrammer/render-markdown.nvim",
-    ft = { "markdown", "codecompanion" },
+    "ravitemer/mcphub.nvim",
   },
+  opts = {
+    strategies = {
+      chat = { adapter = "gemini" },
+      inline = { adapter = "gemini" },
+    },
+  },
+  config = function(_, opts)
+    require("codecompanion").setup(opts)
+
+    local group = vim.api.nvim_create_augroup("CodeCompanionFidget", {})
+    local handles = {}
+    local progress = require("fidget.progress")
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "CodeCompanionRequestStarted",
+      group = group,
+      callback = function(e)
+        handles[e.data.id] = progress.handle.create({
+          title = "CodeCompanion",
+          message = "Thinking...",
+          lsp_client = { name = e.data.adapter.formatted_name },
+        })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "CodeCompanionRequestFinished",
+      group = group,
+      callback = function(e)
+        local h = handles[e.data.id]
+        if h then
+          h.message = e.data.status == "success" and "Done" or "Failed"
+          h:finish()
+          handles[e.data.id] = nil
+        end
+      end,
+    })
+  end,
 }
