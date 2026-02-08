1521d6d6333fce63b6be71b5ae053ee6369bbdbe
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-08 13:50:37 -0300

    feat(plugins): keymaps for codecompanion

--- lua/plugins/feat_aitool.lua
+++ lua/plugins/feat_aitool.lua
@@ -1,5 +1,14 @@
 return {
   "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  keys = {
+    { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CC: Toggle chat panel" },
+    { "<leader>an", "<cmd>CodeCompanionChat<cr>", desc = "CC: New chat" },
+    { "<leader>ap", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "CC: Actions panel" },
+  },
   opts = {
     display = {
       chat = {
@@ -7,12 +16,21 @@ return {
       },
     },
     strategies = {
-      chat = { adapter = "gemini" },
+      chat = {
+        adapter = "gemini",
+        variables = {
+          ["buffer"] = {
+            callback = function()
+              return require("codecompanion.helpers.variables.buffer").callback({
+                filter = function(bufnr)
+                  return vim.api.nvim_buf_is_valid(bufnr) and vim.api.nvim_buf_get_name(bufnr) ~= ""
+                end,
+              })
+            end,
+          },
+        },
+      },
       inline = { adapter = "gemini" },
     },
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
   },
 }
