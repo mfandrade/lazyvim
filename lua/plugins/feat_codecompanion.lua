dc79621d5a1271d3880cefdbc0f840d891e1ae10
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-14 11:08:56 -0300

    refactor(plugins): remove unused options from codecompanion

---
+++ lua/plugins/feat_codecompanion.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "kiddos/gemini.nvim",
+    opts = {},
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    enabled = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "MeanderingProgrammer/render-markdown.nvim",
+      "ravitemer/codecompanion-history.nvim",
+      "ravitemer/mcphub.nvim",
+    },
+    keys = {
+      { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CC: Toggle chat panel" },
+      { "<leader>aA", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "CC: Add selection to context" },
+      { "<leader>an", "<cmd>CodeCompanionChat<cr>", desc = "CC: New chat" },
+      { "<leader>ap", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "CC: Actions panel" },
+    },
+    opts = {
+      display = { chat = { auto_scroll = false } },
+      adapters = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            schema = {
+              model = {
+                default = "gemini-2.0.flash",
+              },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = { adapter = "gemini" },
+        inline = { adapter = "gemini" },
+      },
+      extensions = {
+        history = { enabled = true },
+      },
+    },
+    config = function(opts)
+      require("codecompanion").setup(opts)
+      vim.cmd("cab cc CodeCompanion")
+    end,
+  },
+}
