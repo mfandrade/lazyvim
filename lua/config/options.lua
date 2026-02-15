f42ac6467ccd19f7a25ff4900508414f1d410ac0
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-15 01:53:18 -0300

    style(options): spacing in comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,39 +4,39 @@ vim.g.have_nerd_font = true
 vim.g.snacks_animate = false
 
 local conf = vim.opt
-conf.clipboard = "unnamed"               -- Don't sync with system clipboard
-conf.mouse = "a"                         -- Enable mouse support for all modes
-conf.tildeop = true                      -- Make tilde (~) behave like an operator
-conf.updatetime = 250                    -- Faster completion and trigger for CursorHold
-conf.timeoutlen = 500                    -- Time to wait for a mapped sequence to complete
-conf.scrolloff = 5                       -- Minimum number of lines to keep above/below the cursor
-conf.modeline = true                     -- Modelines are useful for per-file settings
-conf.modelines = 2                       -- How many lines at bottom/top to check
+conf.clipboard = "unnamed"                   -- Don't sync with system clipboard
+conf.mouse = "a"                             -- Enable mouse support for all modes
+conf.tildeop = true                          -- Make tilde (~) behave like an operator
+conf.updatetime = 250                        -- Faster completion and trigger for CursorHold
+conf.timeoutlen = 500                        -- Time to wait for a mapped sequence to complete
+conf.scrolloff = 5                           -- Minimum number of lines to keep above/below the cursor
+conf.modeline = true                         -- Modelines are useful for per-file settings
+conf.modelines = 2                           -- How many lines at bottom/top to check
 
 local line = vim.opt
-line.number = true                       -- Show absolute line number
-line.relativenumber = true               -- Show relative line number
+line.number = true                           -- Show absolute line number
+line.relativenumber = true                   -- Show relative line number
 
 local editor = vim.opt
-editor.cmdheight = 0                     -- Use 0 lines (make more room for code)
-editor.laststatus = 3                    -- Global status bar (just one in background, regardless splits)
-editor.signcolumn = "yes"                -- Always show the sign column to prevent text shifting
-editor.showtabline = 0                   -- Never show the tab line
-editor.showmode = false                  -- Hide mode (e.g. -- INSERT --) as it's often in statusline
-editor.cursorline = true                 -- Highlight the current line
-editor.colorcolumn = "+1"                -- Draw cursorcolumn 1 char after textwidth
+editor.cmdheight = 0                         -- Use 0 lines (make more room for code)
+editor.laststatus = 3                        -- Global status bar (just one in background, regardless splits)
+editor.signcolumn = "yes"                    -- Always show the sign column to prevent text shifting
+editor.showtabline = 0                       -- Never show the tab line
+editor.showmode = false                      -- Hide mode (e.g. -- INSERT --) as it's often in statusline
+editor.cursorline = true                     -- Highlight the current line
+editor.colorcolumn = "+1"                    -- Draw cursorcolumn 1 char after textwidth
 
 local panes = vim.opt
-panes.splitright = true                  -- Vertical splits will automatically be on the right
-panes.splitbelow = true                  -- Horizontal splits will automatically be below
+panes.splitright = true                      -- Vertical splits will automatically be on the right
+panes.splitbelow = true                      -- Horizontal splits will automatically be below
 
 local indent = vim.opt
-indent.expandtab = true                  -- Convert tabs to spaces
-indent.smarttab = true                   -- Make tab inserting smarter
-indent.autoindent = true                 -- Copy indent from current line when starting a new one
-indent.smartindent = true                -- Insert indents automatically in specific contexts
-indent.list = false                      -- Default to not show invisible chars
-indent.listchars = {                     -- How to show invisible chars
+indent.expandtab = true                      -- Convert tabs to spaces
+indent.smarttab = true                       -- Make tab inserting smarter
+indent.autoindent = true                     -- Copy indent from current line when starting a new one
+indent.smartindent = true                    -- Insert indents automatically in specific contexts
+indent.list = false                          -- Default to not show invisible chars
+indent.listchars = {                         -- How to show invisible chars
   tab      = "» ",
   space    = "·",
   trail    = "⨉",
@@ -47,15 +47,15 @@ indent.listchars = {                     -- How to show invisible chars
 }
 
 local wrapping = vim.opt
-wrapping.wrap = false                    -- No wrap long lines by default
-wrapping.textwidth = 120                 -- Length of a single line of text before wrapping (when enabled)
-wrapping.breakindent = true              -- Wrapped lines will retain the same indent
-wrapping.showbreak = "" --"↪ "             -- String to put at the start of lines that have been wrapped
+wrapping.wrap = false                        -- No wrap long lines by default
+wrapping.textwidth = 120                     -- Length of a single line of text before wrapping (when enabled)
+wrapping.breakindent = true                  -- Wrapped lines will retain the same indent
+wrapping.showbreak = "" --"↪ "               -- String to put at the start of lines that have been wrapped
 
 local search = vim.opt
-search.ignorecase = true                 -- Ignore case in search patterns
-search.smartcase = true                  -- Override 'ignorecase' if search pattern contains upper case
-search.inccommand = "nosplit"            -- Show live preview of substitution in a split window
+search.ignorecase = true                     -- Ignore case in search patterns
+search.smartcase = true                      -- Override 'ignorecase' if search pattern contains upper case
+search.inccommand = "nosplit"                -- Show live preview of substitution in a split window
 
 _G.custom_fold_text = function()
   local fs = vim.v.foldstart
@@ -74,22 +74,22 @@ _G.custom_fold_text = function()
 end
 
 local fold = vim.opt
-fold.foldexpr = "v:lua.vim.lsp.foldexpr()"   -- Uses LPS by default
+fold.foldexpr = "v:lua.vim.lsp.foldexpr()"
 fold.foldmethod = "expr"                     -- How the folds are defined
 fold.foldlevel = 99                          -- All fold open by default
---fold.foldlevelstart = 0                      -- Fold the first level at first
+fold.foldlevelstart = 0                      -- Fold the first level at first
 fold.foldnestmax = 4                         -- Too deep nests doesn't make sense 
 fold.foldtext = "v:lua.custom_fold_text()"   -- What to show in the first line
 fold.fillchars:append({ foldopen = "", foldclose = "", foldsep = "", fold = " " })
 
 local undo = vim.opt
-undo.undofile = true                     -- Save undo history to a file so it persists after closing
-undo.undolevels = 10000                  -- Maximum number of changes that can be undone
+undo.undofile = true                         -- Save undo history to a file so it persists after closing
+undo.undolevels = 10000                      -- Maximum number of changes that can be undone
 
 local backup = vim.opt
-backup.autowriteall = true               -- To avoid annoying confirmation dialogs
-backup.backup = false                    -- Don't create a backup file before overwriting a file
-backup.writebackup = false               -- Don't make a backup before overwriting a file
-backup.swapfile = false                  -- Don't use swapfiles (can be risky, but cleaner)
+backup.autowriteall = true                   -- To avoid annoying confirmation dialogs
+backup.backup = false                        -- Don't create a backup file before overwriting a file
+backup.writebackup = false                   -- Don't make a backup before overwriting a file
+backup.swapfile = false                      -- Don't use swapfiles (can be risky, but cleaner)
 
 -- stylua: ignore end
