fd5a535598b54766af05c4887ff3ec56d1da30ea
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-18 23:19:20 -0300

    refactor(plugins): organize rendermarkdown

--- lua/plugins/feat_avante.lua
+++ lua/plugins/feat_avante.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+    "MeanderingProgrammer/render-markdown.nvim",
     {
       "HakonHarnes/img-clip.nvim",
       event = "VeryLazy",
@@ -10,44 +11,16 @@ return {
         default = {
           embed_image_as_base64 = false,
           prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
+          drag_and_drop = { insert_mode = true },
           use_absolute_path = true,
         },
       },
-    },
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = { file_types = { "markdown", "Avante" } },
-      ft = { "markdown", "Avante" },
     },
   },
   build = "make",
   event = "VeryLazy",
   opts = {
-    instructions_file = "agents.md",
     provider = "claude",
-    providers = {
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-sonnet-4-20250514",
-        timeout = 30000, -- Timeout in milliseconds
-        extra_request_body = {
-          temperature = 0.75,
-          max_tokens = 20480,
-        },
-      },
-      moonshot = {
-        endpoint = "https://api.moonshot.ai/v1",
-        model = "kimi-k2-0711-preview",
-        timeout = 30000, -- Timeout in milliseconds
-        extra_request_body = {
-          temperature = 0.75,
-          max_tokens = 32768,
-        },
-      },
-    },
   },
   config = function()
     require("avante").setup({
