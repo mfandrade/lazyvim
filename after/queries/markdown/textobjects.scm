5bf9f1c12e7f4f6f850aa3168dbbce33ecc00ba1
Marcelo F Andrade <mfandrade@gmail.com>
2026-02-25 02:05:47 -0300

    feat: code block operator in markdown

--- after/queries/markdown/textobjects.scm
+++ after/queries/markdown/textobjects.scm
@@ -1,6 +1,16 @@
 ; extends
 
-; Quadruple backticks 
+; Triple backticks (mapped to 'm' globally)
+(fenced_code_block
+  (fenced_code_block_delimiter) @_delim
+  (#eq? @_delim "```")) @triple.outer
+
+(fenced_code_block
+  (fenced_code_block_delimiter) @_delim
+  (code_fence_content) @triple.inner
+  (#eq? @_delim "```"))
+
+; Quadruple backticks (mapped to 'M' globally)
 (fenced_code_block
   (fenced_code_block_delimiter) @_delim
   (#eq? @_delim "````")) @quad.outer
