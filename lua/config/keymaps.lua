f3e6c346869fb93cade8227acc5c5db28b180060
Marcelo de Freitas Andrade <mfandrad@motorola.com>
2026-03-07 16:11:53 -0300

    fix(keymaps): other key for appends is simpler

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -54,18 +54,18 @@ map("0", function()
   end
 end, "Begining of the line", "n", { expr = true })
 
+-- appends
+map("<leader>,", "A,<esc>", "Append ,")
+map("<leader>;", "A;<esc>", "Append ;")
+map("<leader>\\", "A\\<esc>", "Append \\")
+map("<leader>a", "GA", "Append to the end of the buffer")
+
 -- saves and quits
 map("<leader>w", "<cmd>write<cr>", "Save")
 map("<leader>W", "<cmd>noautocmd write<cr>", "Save (no autocmds)")
 map("<leader>q", "<cmd>quit<cr>", "Quit")
 map("<leader>Q", "<cmd>quitall!<cr>", "Quit without saving")
 map("<leader>X", "<cmd>xit!<cr>", "Save and quit")
-
--- appends
-map(",,", "A,<esc>", "Append ,")
-map(";;", "A;<esc>", "Append ;")
-map("\\\\", "A \\<esc>", "Append \\")
-map("AA", "GA", "Append to the end of the buffer")
 
 -- no save to clipboard
 map("x", '"_dl', "Delete right char")
