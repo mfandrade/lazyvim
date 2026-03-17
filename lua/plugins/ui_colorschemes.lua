f3dd74a0c1fe810148e002fcee4884ad4e3fd6c9
Marcelo de Freitas Andrade <mfandrad@motorola.com>
2026-03-17 13:01:57 -0300

    refactor(colorscheme): unneeded highlights groups

--- lua/plugins/ui_colorschemes.lua
+++ lua/plugins/ui_colorschemes.lua
@@ -37,12 +37,11 @@ return {
     "xiyaowong/transparent.nvim",
     event = "VeryLazy",
     opts = {
-      exclude_groups = { "CursorLine" },
+      exclude_groups = { "CursorLine", "LineNr" },
       on_clear = function()
         local strong = "#ffffff"
         local weaker = "#a0a0a0"
         vim.api.nvim_set_hl(0, "CursorLineNr", { fg = strong, bold = true, bg = "none" })
-        vim.api.nvim_set_hl(0, "LineNr", { fg = weaker, bg = "none" })
         vim.api.nvim_set_hl(0, "LineNrAbove", { fg = weaker, bg = "none" })
         vim.api.nvim_set_hl(0, "LineNrBelow", { fg = weaker, bg = "none" })
       end,
